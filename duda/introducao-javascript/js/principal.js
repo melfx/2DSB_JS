6f0bc4506f7a58d38e031d8190552255642a3c9e
melfx <130380863+melfx@users.noreply.github.com>
2023-09-04 11:46:41 -0300

    Add files via upload

---
+++ duda/introducao-javascript/js/principal.js
@@ -0,0 +1,10 @@
+//-------------------------------------------
+//daqui pra baixo começa o js do site
+
+//busca no site pelo titulo e armazena ele dentro da variável titulo
+let titulo = document.querySelector(".titulo");
+//mostra no devtools o conteudo de texto da variável titulo para fins de teste
+console.log(titulo.textContent);
+//altera o conteudo da variável titulo para o texto desejado
+titulo.textContent = "Treina fofo Nutrição";
+
